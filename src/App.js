afdb32f98af7f7edf5410ec68eb48c3e949a4bae
mundoe1999 <estebanmundo1999@gmail.com>
2019-11-17 23:04:41 -0500

    Setup Initial React + LoginBox Component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import LoginBox from './components/LoginBox';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <LoginBox/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LoginBox.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+// Should Import Links
+// Import CSS Page
+
+class LoginBox extends Component {
+  constructor(){
+    super();
+    this.state = {
+      usermail: '',
+      passcode: ''
+    }
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit = e => {
+   // e.preventDefault();
+    // Does call to Login Route
+    // And return result
+    alert(`Email: ${this.state.usermail}\nPass: ${this.state.passcode}`);
+  }
+  render(){
+    return (
+      <div>
+        <form>
+          <input
+            type="email"
+            name="usermail"
+            onChange={this.handleChange}
+            placeholder="Email"
+            required
+          />
+          <br/>
+          <input
+            type="password"
+            name="passcode"
+            onChange={this.handleChange}
+            placeholder="Password"
+            required
+          />
+          <br/>
+
+          <input
+            type="submit"
+            value="Login"
+            onClick={this.handleSubmit}
+          />
+        </form>
+        <button>Create Account</button>
+      </div>
+
+    )
+  }
+}
+
+export default LoginBox;
