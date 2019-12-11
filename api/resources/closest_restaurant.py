31ba0ab6b85eca328469ee89034b21c5f3ae1c23
Elijah Augustin <elijaha@Elijahs-MacBook-Pro.local>
2019-12-10 22:45:01 -0500

    adding more res info

--- api/resources/closest_restaurant.py
+++ api/resources/closest_restaurant.py
@@ -12,7 +12,7 @@ class ClosestRestaurant(Resource):
     def get(self, user_id):
         try:
             get_user_query = """
-                select restaurant_address.restaurant_id ,restaurant_address.street_adr, restaurant_address.cityaddr , restaurant_address.state, restaurant_address.zipcode from restaurant_address join user_address on user_address.zipcode > restaurant_address.zipcode-2000 and user_address.zipcode < restaurant_address.zipcode+2000 where user_id = {} order by abs(user_address.zipcode - restaurant_address.zipcode) asc limit 5;
+                select restaurants.name, restaurants.rating, restaurant_address.restaurant_id ,restaurant_address.street_adr, restaurant_address.cityaddr , restaurant_address.state, restaurant_address.zipcode, restaurant_address.latitude, restaurant_address.longitude from restaurant_address join user_address on user_address.zipcode > restaurant_address.zipcode-2000 and user_address.zipcode < restaurant_address.zipcode+2000 JOIN restaurants ON restaurants.restaurant_id = restaurant_address.restaurant_id where user_id = {} order by abs(user_address.zipcode - restaurant_address.zipcode) asc limit 5;
             """.format(user_id)
             print(get_user_query)
             cn = connection()
@@ -21,17 +21,19 @@ class ClosestRestaurant(Resource):
 
             restaurants = []
 
-            for (restaurant_id, street_adr, cityaddr, state, zipcode) in cur:
+            for (name, rating, restaurant_id, street_adr, cityaddr, state, zipcode, latitude, longitude) in cur:
                 restaurant = {
+                    'name': name,
+                    'rating': rating,
                     'restaurant_id': restaurant_id,
                     'street_adr': street_adr,
                     'cityaddr': cityaddr,
                     'state': state,
-                    'zipcode': zipcode
+                    'zipcode': zipcode,
+                    'latitude': float(latitude),
+                    'longitude': float(longitude)
                         }
                 restaurants.append(restaurant)
             return {"restaurants": restaurants}, 200
-        except ValidationError as err:
-            return err.messages, 500
         except:
             return {"Msg": "Some error occurred"}, 500
